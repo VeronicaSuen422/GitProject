c2cc744c8cef45b82569b79287606254500385d7
Suen Pui Ka <alza004422@gmail.com>
2023-10-04 01:25:39 -0400

    repeat gameplay

--- testing.c
+++ testing.c
@@ -22,34 +22,44 @@ int main() {
    char computer; // Store computer's input
    char result; // Store the result
    int n; // Store the random number used for computer's input
+   char tryagain; // Decide whether the user want to play again
 
-   // User input
-   printf("p for Paper\nv for Scissors\ns for Stone\nWhat is your input? ");
-   scanf("%c", &user);
+   do {
 
-   // Generate computer's input
-   srand(time(NULL));
-   n = rand() % 100;
-   if (n < 33) {
-      computer = 'p'; // Paper
-      printf("Computer input is Paper\n");
-   } else if (n >= 33 && n < 66) {
-      computer = 'v'; // Scissors
-      printf("Computer input is Scissors\n");
-   } else {
-      computer = 's'; // Stone
-      printf("Computer input is Stone\n");
-   }
+      fflush(stdin);
 
-   // Show result
-   result = gameplay(user, computer);
-   if (result == 'u') {
-      printf("User wins!");
-   } else if (result == 'c') {
-      printf("Computer wins!");
-   } else {
-      printf("Draw");
-   }
-   
+      // User input
+      printf("p for Paper\nv for Scissors\ns for Stone\nWhat is your input? ");
+      scanf("%c", &user);
+
+      // Generate computer's input
+      srand(time(NULL));
+      n = rand() % 100;
+      if (n < 33) {
+         computer = 'p'; // Paper
+         printf("Computer input is Paper\n");
+      } else if (n >= 33 && n < 66) {
+         computer = 'v'; // Scissors
+         printf("Computer input is Scissors\n");
+      } else {
+         computer = 's'; // Stone
+         printf("Computer input is Stone\n");
+      }
+
+      // Show result
+      result = gameplay(user, computer);
+      if (result == 'u') {
+         printf("User wins!\n");
+      } else if (result == 'c') {
+         printf("Computer wins!\n");
+      } else {
+         printf("Draw\n");
+      }
+
+      // Ask for Replay
+      printf("y for Yes\nn for No\nDo you want to play again? ");
+      scanf("%s", &tryagain);
+   } while (tryagain == 'y');
+
    return 0;
 }
